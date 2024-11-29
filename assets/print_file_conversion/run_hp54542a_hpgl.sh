b23b7467cce53286209cbca9ba4d18806a19fcdc
Tom Verbeure <hombre+github.com@gmail.com>
2024-11-29 15:13:11 -0800

    Squashed commit of the following:
    commit 1e034ed173424cc759e2ff3a03a1121f7f4b6acd
    Author: Tom Verbeure <hombre+github.com@gmail.com>
    Date:   Fri Nov 29 15:10:59 2024 -0800
    
        Siglent. Pictures.
    
    commit 4fb88a246b57d84628d047a4e55cd16ab4619670
    Author: Tom Verbeure <hombre+github.com@gmail.com>
    Date:   Mon Apr 22 21:33:36 2024 -0700
    
        Update 8753C plots.
    
    commit 40b18d9faeebfb56e17e8e70762301c3f53ff1a1
    Author: Tom Verbeure <hombre+github.com@gmail.com>
    Date:   Mon Apr 22 21:26:38 2024 -0700
    
        Replace HP 8753C screenshots.
    
    commit a942fd43252323fb62301e89ddc102805c566420
    Author: Tom Verbeure <hombre+github.com@gmail.com>
    Date:   Mon Apr 22 20:55:14 2024 -0700
    
        Siglent WIP.
    
    commit 6331811287ec231b0424f1c9170f6b51b5fde8ae
    Author: Tom Verbeure <hombre+github.com@gmail.com>
    Date:   Sun Apr 21 12:54:20 2024 -0700
    
        Title.
    
    commit 6f9911196dfbfaeaada748f23153085a53f6805f
    Author: Tom Verbeure <hombre+github.com@gmail.com>
    Date:   Sun Apr 21 12:52:37 2024 -0700
    
        R3273 and HP 8753C.
    
    commit 98aca0e685eb62d72f5749b6959900a47017acdc
    Author: Tom Verbeure <hombre+github.com@gmail.com>
    Date:   Sat Apr 20 23:41:14 2024 -0700
    
        ImageMagick.
    
    commit 960345fd3e775a08bca4289f04eaee1d5a531ee6
    Author: Tom Verbeure <hombre+github.com@gmail.com>
    Date:   Sat Apr 20 23:18:50 2024 -0700
    
        TDS 684B output.
    
    commit 98df7c5d62539420750b9f3f2480a93efe5c6233
    Author: Tom Verbeure <hombre+github.com@gmail.com>
    Date:   Sat Apr 20 21:33:52 2024 -0700
    
        Finish HP 54521A.
    
    commit 10bddffb46bc505675a1e99cbe44c933c9a93ac4
    Author: Tom Verbeure <hombre+github.com@gmail.com>
    Date:   Sun Apr 14 23:38:42 2024 -0700
    
        HP 54542A.
    
    commit 2c74b1db6275cdce744746fdf97929d7b254d7b5
    Author: Tom Verbeure <hombre+github.com@gmail.com>
    Date:   Sun Apr 14 21:08:36 2024 -0700
    
        Minor edit.
    
    commit 1dfb10366b045dc013a64780462d43f3db2df4f8
    Author: Tom Verbeure <hombre+github.com@gmail.com>
    Date:   Sun Apr 14 21:00:21 2024 -0700
    
        Click to download.
    
    commit cdb96344a697ced1d76dd87bd5f616a5735417a7
    Author: Tom Verbeure <hombre+github.com@gmail.com>
    Date:   Sun Apr 14 20:57:19 2024 -0700
    
        TDS540

---
+++ assets/print_file_conversion/run_hp54542a_hpgl.sh
@@ -0,0 +1,2 @@
+~/projects/fake_parallel_printer/fake_printer.py -i -p /dev/ttyACM0 -f hp54542a_ -s plotter.hpgl -v
+hp2xx -m png -a 1.5 --width 250 --height 250 -c 12345671 -p 11111111 hp54542a_0.plotter.hpgl
